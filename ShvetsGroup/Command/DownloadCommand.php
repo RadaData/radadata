fc5e8879462bce9113aeab9f6ee6bd98c6c1ff13
Alexander Shvets <neochief@shvetsgroup.com>
2015-06-24 22:08:17 +0300

    Refactored all the code.

--- ShvetsGroup/Command/DownloadCommand.php
+++ ShvetsGroup/Command/DownloadCommand.php
@@ -67,17 +67,21 @@ class DownloadCommand extends Console\Command\Command
         try {
             $html = download('/laws/card/' . $law_id, false, '/laws/show/' . $law_id . '/card');
 
+            mark_law($law_id, DOWNLOADED_CARD);
+
             if (strpos($html, 'Текст відсутній') !== false || strpos($html, 'Текст документа') === false) {
-                mark_law($law_id, DOWNLOADED, NO_TEXT);
-            } else {
-                $url = '/laws/show/' . $law_id . '/page';
-                $html = download($url, false, null, ['required' => ['<div id="article"', '</body>']]);
-                while (preg_match('|<a href="?(.*?)"? title="наступна сторінка">наступна сторінка</a>|', $html, $matches)) {
-                    $url = urldecode($matches[1]);
-                    $html = download($url, false, null, ['required' => ['<div id="article"', '</body>']]);
-                }
-                mark_law($law_id, DOWNLOADED, HAS_TEXT);
+                mark_law($law_id, DOWNLOADED_CARD, NO_TEXT);
             }
+
+            //else {
+            //    $url = '/laws/show/' . $law_id . '/page';
+            //    $html = download($url, false, null, ['required' => ['<div id="article"', '</body>']]);
+            //    while (preg_match('|<a href="?(.*?)"? title="наступна сторінка">наступна сторінка</a>|', $html, $matches)) {
+            //        $url = urldecode($matches[1]);
+            //        $html = download($url, false, null, ['required' => ['<div id="article"', '</body>']]);
+            //    }
+            //    mark_law($law_id, DOWNLOADED_REVISIONS, HAS_TEXT);
+            //}
         } catch (Exception $e) {
             _log($e->getMessage(), 'red');
         }
