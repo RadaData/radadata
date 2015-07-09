d2a42b559ad96bf209337857b24e3e9cc16bf114
Alexander Shvets <neochief@shvetsgroup.com>
2015-07-09 17:10:43 +0300

    Refactored all the code.

--- src/ShvetsGroup/Command/CheckCommand.php
+++ src/ShvetsGroup/Command/CheckCommand.php
@@ -57,6 +57,13 @@ class CheckCommand extends Console\Command\Command
             ]);
         }
 
+        function is_403($html)
+        {
+            return downloader()->detectFakeContent($html, [
+                'stop' => ['Error 403', 'Доступ заборонено', 'Ваш IP автоматично заблоковано']
+            ]);
+        }
+
         function remove_dir($dir)
         {
             exec('rm -rf ' . $dir);
@@ -64,12 +71,13 @@ class CheckCommand extends Console\Command\Command
 
         $result_count = Law::where('status', '<', Law::SAVED)->count();
         $nd_orphaned_dirs = 0;
+        $d_broken_card = 0;
         $d_no_files = 0;
         $d_fake_content = 0;
         $d_unknown_text_true_content = 0;
         $d_unknown_text_no_text = 0;
         $i = 1;
-        Law::where('status', '<', Law::SAVED)->orderBy('id')->chunk(200, function($laws) use ($fix, $law_dir, &$i, &$result_count, &$nd_orphaned_dirs, &$d_no_files, &$d_fake_content, &$d_unknown_text_true_content, &$d_unknown_text_no_text) {
+        Law::where('status', '<', Law::SAVED)->orderBy('id')->chunk(200, function($laws) use ($fix, $law_dir, &$i, &$result_count, &$nd_orphaned_dirs, &$d_broken_card, &$d_no_files, &$d_fake_content, &$d_unknown_text_true_content, &$d_unknown_text_no_text) {
             foreach ($laws as $law) {
                 $law_path = $law_dir . $law->id;
                 $card_path = $law_dir . $law->id . '/card.html';
@@ -82,6 +90,14 @@ class CheckCommand extends Console\Command\Command
                         remove_dir($law_path);
                     }
                     continue;
+                }
+
+                if ($law->status > Law::NOT_DOWNLOADED && file_exists($card_path) && is_403(file_get_contents($card_path))) {
+                    $d_broken_card++;
+                    if ($fix) {
+                        remove_dir($law_path);
+                        $law->update(['status' => Law::NOT_DOWNLOADED]);
+                    }
                 }
 
                 if ($law->status >= Law::DOWNLOADED_REVISIONS && $law->has_text == Law::HAS_TEXT && !file_exists($text_path) && !file_exists($page_path)) {
@@ -141,6 +157,7 @@ class CheckCommand extends Console\Command\Command
         print("\n" . 'Not downloaded : ' . $not_downloaded);
         print("\n" . '-------------------------------------------------');
         print("\n" . 'Junk directories           : ' . $nd_orphaned_dirs);
+        print("\n" . 'Broken card page           : ' . $d_broken_card);
         print("\n" . 'Missing files for downloads: ' . $d_no_files);
         print("\n" . 'Fake content for downloads : ' . $d_fake_content);
         print("\n" . 'Has text, but not marked   : ' . $d_unknown_text_true_content);
